777a2efb398305838adcae2e2ee73a1984bc86df
shabobble <pksullivan@yahoo.com>
2021-06-16 14:29:56 -0400

    functionality added to add new weapon items to database

--- controllers/api/weaponRoutes.js
+++ controllers/api/weaponRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Weapon } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Weapon.findAll({})
@@ -9,5 +10,40 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/', withAuth, (req, res) => {
+    Weapon.create({
+        mainEffect: req.body.mainEffect,
+        majorEffect: req.body.majorEffect,
+        minorEffect: req.body.minorEffect,
+        weaponType: req.body.weaponType,
+        capsValue: req.body.capsValue,
+        user_id: req.session.user_id
+    })
+    .then(weaponData => res.json(weaponData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Weapon.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(weaponData => {
+        if(!weaponData) {
+            res.status(404).json({ message: 'No weapon found with this id' });
+            return;
+        }
+        res.json(weaponData);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router;
