4c8d0616af7487f795dce07c454ef05564b1563e
shabobble <pksullivan@yahoo.com>
2021-06-16 14:32:11 -0400

    ability to navigate to pages to add weapons/armor

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,7 +3,22 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
     res.render('dashboard', {
-        loggedIn: req.session.loggedIn
+        loggedIn: req.session.loggedIn,
+        user_id: req.session.user_id
+    })
+})
+
+router.get('/add-weapon', withAuth, (req, res) => {
+    res.render('add-weapon', {
+        loggedIn: req.session.loggedIn,
+        user_id: req.session.user_id
+    })
+})
+
+router.get('/add-armor', withAuth, (req, res) => {
+    res.render('add-armor', {
+        loggedIn: req.session.loggedIn,
+        user_id: req.session.user_id
     })
 })
 
